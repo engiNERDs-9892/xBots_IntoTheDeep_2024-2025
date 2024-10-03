520325138d36a43e7e5461d00b0e081ba427a4f6
enginerds <enginerds@tccs.org>
2024-10-03 12:34:41 -0700

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Basic.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="YOUR NAME HERE", group="Iterative Opmode")
+public class Auto_Basic extends LinearOpMode {
+
+//Initialize the code - Get it ready to run
+
+    //Create Motor/Servo/Sensor List
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
+
+
+    public final void runOpMode(){
+        //Create Hardware map so code can send signals to the correct ports
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+
+
+        //Set Motor Power to 0 to start
+        motorFL.setPower(0);
+        motorBL.setPower(0);
+        motorFR.setPower(0);
+        motorBR.setPower(0);
+
+
+        //Set the Motor Directions
+        motorFL.setDirection(DcMotor.Direction.REVERSE);
+        motorFR.setDirection(DcMotor.Direction.FORWARD);
+        motorBR.setDirection(DcMotor.Direction.FORWARD);
+        motorBL.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // Tell the Code to wait to start after initialization
+        waitForStart();
+
+        // Give it commands to run
+
+
+
+    }// ends public final void runOpMode
+}//LinearOpMode
