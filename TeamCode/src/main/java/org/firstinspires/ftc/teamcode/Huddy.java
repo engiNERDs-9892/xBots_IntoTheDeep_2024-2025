6de1038c155dee6bb40bde0ab1f1e2d4e95e12a7
EngiNERDS <Enginerds@tccs.org>
2024-10-15 14:19:48 -0700

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Huddy.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name="Huddy CODE", group="Iterative Opmode")
+public class Huddy extends LinearOpMode {
+
+//Initialize the code - Get it ready to run
+    //If you read this and are about to code you are in for a doozy.
+
+    //Create Motor/Servo/Sensor List
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
+
+
+    public final void runOpMode(){
+        //Create Hardware map so code can send signals to the correct ports
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+
+
+        int in = 45;
+
+        //Set Motor Power to 0 to start
+        motorFL.setPower(0);
+        motorBL.setPower(0);
+        motorFR.setPower(0);
+        motorBR.setPower(0);
+
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        // Tell the Code to wait to start after initialization
+        waitForStart();
+
+        // Give it commands to run during auto;
+        Forward(1080,.5);
+
+
+    }// ends public final void runOpMode
+
+
+
+
+    //ENTER FUNCTIONS HERE
+
+
+    public void Forward(int target, double speed){
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        motorBL.setTargetPosition(target);
+        motorBR.setTargetPosition(target);
+        motorFL.setTargetPosition(target);
+        motorFR.setTargetPosition(target);
+
+        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorFL.setPower(speed);
+        motorFR.setPower(speed);
+        motorBL.setPower(speed);
+        motorBR.setPower(speed);
+        while(opModeIsActive() && (motorBL.isBusy())) {
+
+        }
+
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+    }// Closes forward function
+
+}//LinearOpMode
